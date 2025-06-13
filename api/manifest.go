cd95b49c73618516fb6ed316a0082574c504656b
Antti Kivi <antti@anttikivi.fi>
2025-06-14 00:43:15 +0300

    feat: add the commands and configs to the manifest type

--- api/manifest.go
+++ api/manifest.go
@@ -14,12 +14,42 @@
 
 package api
 
+// The supported value types for a KeyValue.
+const (
+	BoolValue   ValueType = "bool"
+	IntValue    ValueType = "int"
+	StringValue ValueType = "string"
+)
+
+// ValueType is used as the type indicator of a KeyValue.
+type ValueType string
+
 // A Manifest is the program representation of a plugin manifest.
 type Manifest struct {
-	Name        string `json:"name"`
-	Domain      string `json:"domain"`
+	// Name is the human-readable name of the plugin. It must be unique.
+	Name string `json:"name"`
+
+	// Domain is the domain of the plugin that is used to identify the plugin in
+	// commands and tasks. It must be unique.
+	//
+	// All of the commands defined by the plugin are subcommands of the domain.
+	// The domain is also used as a prefix for the tasks provided by the plugin.
+	Domain string `json:"domain"`
+
+	// Description is the description of the plugin that is shown to the user in
+	// the help message.
 	Description string `json:"description"`
-	Executable  string `json:"executable"`
+
+	// Executable is the name of the executable file of the plugin in
+	// the plugin's directory.
+	Executable string `json:"executable"`
+
+	// Config is a list of ConfigEntries that are used to define
+	// the configuration of the plugin.
+	Config []ConfigEntry `json:"config,omitempty"`
+
+	// Commands is a list of Commands that this plugin provides.
+	Commands []Command `json:"commands,omitempty"`
 }
 
 // A Command is the program representation of a plugin command that is defined
@@ -39,6 +69,14 @@ type Command struct {
 	// Description is the description of the command that is shown to the user
 	// in the help message.
 	Description string `json:"description"`
+
+	// Aliases is a list of aliases for the command that can be used instead of
+	// Name to run this command.
+	Aliases []string `json:"aliases,omitempty"`
+
+	// Config is a list of ConfigEntries that are used to define
+	// the configuration of the command.
+	Config []ConfigEntry `json:"config,omitempty"`
 }
 
 // A Flag is a command-line flag the is defined in the manifest for a plugin
@@ -62,14 +100,28 @@ type Flag struct {
 	// Description is the description of the flag that is shown to the user in
 	// the help message.
 	Description string `json:"description"`
-	Default     string `json:"default"`
-	Required    bool   `json:"required"`
+
+	// TODO: Add inverse flag for booleans.
 }
 
+// A KeyValue is a key-value pair that is used to define a config value in the
+// manifest. Depending on the context, it is used either as a part of
+// a ConfigEntry or as is.
 type KeyValue struct {
-	Key   string `json:"key"`
-	Value any    `json:"value"`
-	Type  string `json:"type"`
+	// Key is the key of the KeyValue as it would be written in, for example,
+	// the config file.
+	Key string `json:"key"`
+
+	// Value is the current value of KeyValue as the type it should be defined
+	// as. When the plugin provides the initial KeyValue, for example, as a part
+	// of the manifest, Value should contain the default value of the KeyValue.
+	// When KeyValue is used to send data from Reginald to the plugin, Value
+	// contains the current value of the KeyValue.
+	Value any `json:"value"`
+
+	// Type is a string representation of the type of the value that this
+	// KeyValue holds.
+	Type ValueType `json:"type"`
 }
 
 // A ConfigEntry is a configuration entry that is defined in the manifest. It
@@ -79,8 +131,24 @@ type KeyValue struct {
 // variable for the value of the ConfigEntry. These behaviors can be overridden
 // with the fields provided in the ConfigEntry.
 type ConfigEntry struct {
-	Name        string `json:"name"`
-	Description string `json:"description"`
-	Type        string `json:"type"`
-	Default     string `json:"default"`
+	KeyValue
+
+	// Flag contains the information on the possible command-line flag that is
+	// associated with this ConfigEntry. Flag must be nil if the ConfigEntry has
+	// no associated flag. Otherwise, its type must match [Flag].
+	Flag *Flag `json:"flag,omitempty"`
+
+	// EnvOverride optionally defines a string to use in the environment
+	// variable name instead of the automatic name of the variable that will be
+	// composed using the Key in the embedded [KeyValue]. It is appended after
+	// the prefix `REGINALD_` but if EnvOverride is used to set the name of
+	// the environment variable, the name of the plugin or the name of
+	// the command is not added to variable name automatically.
+	EnvOverride string `json:"envOverride,omitempty"`
+
+	// FlagOnly tells Reginald whether this ConfigEntry should only be
+	// controlled by a command-line flag. If this is set to true, Reginald won't
+	// read the value of this ConfigEntry from the config file or from
+	// environment variables.
+	FlagOnly bool `json:"flagOnly,omitempty"`
 }
